bdb9d282c900664c250db7f0d1b948ccb2dfbb33
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2020-12-15 23:09:22 +0200

    init

--- src/App.vue
+++ src/App.vue
@@ -10,9 +10,6 @@
 // @ is an alias to /src
 import NavBar from '@/components/NavBar.vue'
 import Footer from '@/components/Footer.vue'
-import Footer from '@/components/Footer.vue'
-import Footer from '@/components/Footer.vue'
-// import Footer from '@/components/Footer.vue'
 
 export default {
   components: {
